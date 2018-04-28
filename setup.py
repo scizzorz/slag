eb6bba636914d0036d70bc1017a9a391cb802ff8
John Weachock <jweachock@gmail.com>
2018-04-27 22:49:02 -0400

    Try fixing data junk AGAIN
    Python packaging sucks I think

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import setuptools
+import os
+
+def walk(path):
+  files = [os.path.join(path, f) for f in os.listdir(path)]
+  return (path, files)
 
 
 setuptools.setup(
   name='slag',
-  version="0.1.2",
+  version="0.1.3",
   description='A distributed micro-blog social network on the block chain.',
   long_description=open('README.md').read().strip(),
   author='John Weachock',
@@ -11,6 +16,10 @@ setuptools.setup(
   url='https://github.com/scizzorz/slag',
   py_modules=['slag'],
   scripts=['bin/slag'],
+  data_files=[
+    walk('html'),
+    walk('css'),
+  ],
   install_requires=list(l.strip() for l in open('requirements.txt') if l),
   include_package_data=True,
   license='MIT License',
