a4c328cdfd7172bacd072476e548e497106141ac
John Weachock <jweachock@gmail.com>
2018-04-27 22:36:14 -0400

    Try including css/ and html/
    WHY PYPI WHY

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
   name='slag',
-  version="0.1.0",
+  version="0.1.1",
   description='A distributed micro-blog social network on the block chain.',
   long_description=open('README.md').read().strip(),
   author='John Weachock',
