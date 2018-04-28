2dda685160cf8c23157842ef6bf40002c12c5226
John Weachock <jweachock@gmail.com>
2018-04-27 21:45:38 -0400

    Add setup.py
    Python packaging is hard

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+  name='slag',
+  version="0.1.0",
+  description='A distributed micro-blog social network on the block chain.',
+  long_description=open('README.md').read().strip(),
+  author='John Weachock',
+  author_email='jweachock@gmail.com',
+  url='https://github.com/scizzorz/slag',
+  py_modules=['slag'],
+  scripts=['bin/slag'],
+  install_requires=list(l.strip() for l in open('requirements.txt') if l),
+  include_package_data=True,
+  license='MIT License',
+  zip_safe=False,
+  keywords='git blog',
+)
